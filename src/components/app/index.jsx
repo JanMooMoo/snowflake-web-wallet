5f2a16e0ca810ce0e18a9daf53b18dea5e44346b
ux33-331 <61588635+ux33-331@users.noreply.github.com>
2020-10-29 14:35:57 +0100

    cleanup

--- src/components/app/index.jsx
+++ src/components/app/index.jsx
@@ -13,8 +13,6 @@ import {
 import Header from '../header';
 import Wallet from '../../routes/wallet';
 import Sidebar from '../sidebar';
-import Identity from '../../routes/identity';
-
 
 function App() {
   return (
@@ -22,16 +20,13 @@ function App() {
   <div>
     <Header />
     <Container fluid className="app">
-
       <Row>
-
         <Col xs="12" sm="12" md="12" lg="12" xl="12" className="app__content">
         <Col className="new-sidebar">
         <Sidebar />
         </Col>
           <Switch>
             <Route exact path="/wallet" component={Wallet} />
-            <Route exact path="/identity" component={Identity} />
           </Switch>
         </Col>
       </Row>
